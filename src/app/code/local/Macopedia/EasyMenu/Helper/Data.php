1d456e9486225916fea45a37ef4177a0324c1825
Agata <a.firlejczyk@macopedia.pl>
2014-04-07 15:51:14 +0000

    Add system configuration. Show menu depends on configuration

--- src/app/code/local/Macopedia/EasyMenu/Helper/Data.php
+++ src/app/code/local/Macopedia/EasyMenu/Helper/Data.php
@@ -2,5 +2,8 @@
 
 class Macopedia_EasyMenu_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
+    public function isEnabled()
+    {
+        return Mage::getStoreConfigFlag('easymenu/options/enable');
+    }
 }
