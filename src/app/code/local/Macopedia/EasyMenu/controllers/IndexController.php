3d6dbd066263f71f3adff4b8b1e4554136c4d131
Agata <a.firlejczyk@macopedia.pl>
2014-04-10 11:22:32 +0200

    Remove unnecessary frontend controller

--- src/app/code/local/Macopedia/EasyMenu/controllers/IndexController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Macopedia_EasyMenu_IndexController extends Mage_Core_Controller_Front_Action
-{
-   	public function indexAction(){
-   		$this->loadLayout();
-        $this->renderLayout();
-   	}
-}
