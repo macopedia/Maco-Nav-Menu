75bf883c906b43ef3279d4a014210601d53b2fa4
Agata <a.firlejczyk@macopedia.pl>
2014-04-28 12:24:08 +0200

    Change method description

--- src/app/code/local/Macopedia/EasyMenu/Helper/Data.php
+++ src/app/code/local/Macopedia/EasyMenu/Helper/Data.php
@@ -29,7 +29,7 @@ class Macopedia_EasyMenu_Helper_Data extends Mage_Core_Helper_Abstract
     }
 
     /**
-     * Get default menu
+     * Get default store id
      *
      * @return mixed
      */
